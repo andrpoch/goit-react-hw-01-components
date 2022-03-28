cb5e408f50ef2a6febdb857dec4ba73c5acf6a7c
andrpoch <andrpoch25@gmail.com>
2022-03-28 22:28:08 +0300

    complete profile, statistics, friendList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,28 @@
+import React from 'react';
+
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
+
+import Statistics from './Statistics/Statistics';
+import data from '../data/data.json';
+
+import FriendList from './FriendList/FriendList';
+import friends from "../data/friends.json";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div className="container">
+  <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
+
+
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes, { string } from 'prop-types';
+import styles from './FriendList.module.css';
+
+const FriendList = ({ friends }) => (
+   <ul className={styles.friend_list}>
+      {friends.map(friend => (<li className={styles.item} key={friend.id}>
+         <span className={friend.isOnline ? styles.isOnline : styles.isOffline}></span>
+         <img className={styles.avatar} src={friend.avatar} alt="User avatar" width="48" />
+         <p className={styles.name}>{friend.name}</p>
+      </li>))}
+   </ul>);
+
+FriendList.propTypes = {
+   friends: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+   })
+};
+
+export default FriendList;
