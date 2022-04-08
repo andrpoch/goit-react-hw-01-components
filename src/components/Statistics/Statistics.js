95edb741410df61eb3b04afe50d94c8591e07671
andrpoch <andrpoch25@gmail.com>
2022-04-08 13:33:44 +0300

    fix

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -17,6 +17,7 @@ const Statistics = ({ title, stats }) => (
 );
 
 Statistics.propTypes = {
+     title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
